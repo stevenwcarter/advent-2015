2662faa7ce3aec6efabc06ec0cdab8b96c80f3c8
Steven Carter <steven.carter@bounteous.com>
2022-12-24 13:56:20 -0500

    Adding 5 and 6

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -1,25 +1,109 @@
-use advent_2015::read_lines;
+/// Santa needs help figuring out which strings in his text file are naughty or nice.
+
+/// A nice string is one with all of the following properties:
+///
+/// It contains at least three vowels (aeiou only), like aei, xazegov, or aeiouaeiouaeiou.
+/// It contains at least one letter that appears twice in a row, like xx, abcdde (dd), or aabbccdd (aa, bb, cc, or dd).
+/// It does not contain the strings ab, cd, pq, or xy, even if they are part of one of the other requirements.
+/// For example:
+///
+/// ugknbfddgicrmopn is nice because it has at least three vowels (u...i...o...), a double letter (...dd...), and none of the disallowed substrings.
+/// aaa is nice because it has at least three vowels and a double letter, even though the letters used by different rules overlap.
+/// jchzalrnumimnmhp is naughty because it has no double letter.
+/// haegwjzuvuyypxyu is naughty because it contains the string xy.
+/// dvszwmarrgswjxmb is naughty because it contains only one vowel.
 
 fn main() {
-    let puzzle_data = "puzzles/11.txt";
+    let puzzle_data = "puzzles/5.txt";
 
-    let answer_a = a(puzzle_data);
+    let input: String = std::fs::read_to_string(puzzle_data).unwrap();
+
+    let answer_a = a(&input);
     println!("A Score: {}", answer_a);
 
-    let b_result = b(puzzle_data);
+    let b_result = b(&input);
     println!("B Score: {}", b_result);
 }
 
-fn a(path: &str) -> u32 {
-    read_lines(path).unwrap().iter().for_each(|_line| {
-        //
-    });
-
-    0
+fn vowel_test(line: &&str) -> bool {
+    line.chars().filter(|c| "aeiou".contains(*c)).count() >= 3
 }
 
-fn b(_path: &str) -> u32 {
-    0
+fn double_letter_test(line: &&str) -> bool {
+    let mut found = false;
+    let checks = vec![
+        "aa", "bb", "cc", "dd", "ee", "ff", "gg", "hh", "ii", "jj", "kk", "ll", "mm", "nn", "oo",
+        "pp", "qq", "rr", "ss", "tt", "uu", "vv", "ww", "xx", "yy", "zz",
+    ];
+    for check in checks {
+        if line.contains(check) {
+            found = true;
+        }
+    }
+
+    found
+}
+
+fn missing_naughty_letters(line: &&str) -> bool {
+    // ab, cd, pq, or xy,
+    let mut nice = true;
+    let checks = vec!["ab", "cd", "pq", "xy"];
+    for check in checks {
+        if line.contains(check) {
+            nice = false;
+        }
+    }
+    nice
+}
+
+fn a(input: &str) -> usize {
+    input
+        .split('\n')
+        .into_iter()
+        .filter(vowel_test)
+        .filter(double_letter_test)
+        .filter(missing_naughty_letters)
+        .count()
+}
+
+fn letter_pair_test(line: &&str) -> bool {
+    let checks = vec![
+        "aa", "bb", "cc", "dd", "ee", "ff", "gg", "hh", "ii", "jj", "kk", "ll", "mm", "nn", "oo",
+        "pp", "qq", "rr", "ss", "tt", "uu", "vv", "ww", "xx", "yy", "zz",
+    ];
+
+    let mut nice = false;
+    for check in checks {
+        if !nice {
+            let pos1 = line.find(check);
+            if let Some(pos1) = pos1 {
+                let pos2 = line[pos1 + 2..].find(check);
+                if pos2.is_some() {
+                    nice = true;
+                }
+            }
+        }
+    }
+
+    nice
+}
+// fn letter_repeats_one_between(line: &&str) -> bool {
+//     let checks = vec![
+//         "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",
+//         "s", "t", "u", "v", "w", "x", "y", "z",
+//     ];
+//     let line_iter = line.chars().peekable();
+//     let mut nice = false;
+// }
+
+fn b(input: &str) -> usize {
+    input
+        .split('\n')
+        .into_iter()
+        .filter(vowel_test)
+        .filter(double_letter_test)
+        .filter(missing_naughty_letters)
+        .count()
 }
 
 #[cfg(test)]
@@ -28,14 +112,18 @@ mod tests {
 
     #[test]
     fn test_a() {
-        let test_path = "test-resources/day11.txt";
-        let result = a(test_path);
-        assert_eq!(result, 157);
+        assert!(vowel_test(&"aaa"));
+        assert!(double_letter_test(&"aaa"));
+        assert!(missing_naughty_letters(&"aaa"));
+        assert_eq!(
+            a("aaa\nugknbfddgicrmopn\njchzalrnumimnmhp\nhaegwjzuvuyypxyu\ndvszwmarrgswjxmb"),
+            2
+        );
     }
 
     #[test]
     fn test_b() {
-        let test_path = "test-resources/day11.txt";
+        let test_path = "test-resources/day5.txt";
         let result = b(test_path);
         assert_eq!(result, 70);
     }
--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -1,7 +1,10 @@
+use std::collections::HashMap;
+
 use advent_2015::read_lines;
+use min_max::{max, min};
 
 fn main() {
-    let puzzle_data = "puzzles/11.txt";
+    let puzzle_data = "puzzles/6.txt";
 
     let answer_a = a(puzzle_data);
     println!("A Score: {}", answer_a);
@@ -10,16 +13,162 @@ fn main() {
     println!("B Score: {}", b_result);
 }
 
-fn a(path: &str) -> u32 {
-    read_lines(path).unwrap().iter().for_each(|_line| {
-        //
+fn a(path: &str) -> usize {
+    let mut grid: HashMap<(u32, u32), bool> = HashMap::new();
+    read_lines(path).unwrap().iter().for_each(|line| {
+        let command_parts = line.split(' ').collect::<Vec<&str>>();
+        let command = command_parts.get(1).unwrap();
+        match *command {
+            "on" => {
+                let start = command_parts[2];
+                let end = command_parts[4];
+                let start_parts = start
+                    .split(',')
+                    .map(|a| a.parse::<u32>().unwrap())
+                    .collect::<Vec<u32>>();
+                let end_parts = end
+                    .split(',')
+                    .map(|a| a.parse::<u32>().unwrap())
+                    .collect::<Vec<u32>>();
+                let sx = start_parts.first().unwrap();
+                let sy = start_parts.get(1).unwrap();
+                let ex = end_parts.first().unwrap();
+                let ey = end_parts.get(1).unwrap();
+                for x in *min(sx, ex)..=*max(sx, ex) {
+                    for y in *min(sy, ey)..=*max(sy, ey) {
+                        grid.entry((x, y)).and_modify(|v| *v = true).or_insert(true);
+                    }
+                }
+            }
+            "off" => {
+                let start = command_parts[2];
+                let end = command_parts[4];
+                let start_parts = start
+                    .split(',')
+                    .map(|a| a.parse::<u32>().unwrap())
+                    .collect::<Vec<u32>>();
+                let end_parts = end
+                    .split(',')
+                    .map(|a| a.parse::<u32>().unwrap())
+                    .collect::<Vec<u32>>();
+                let sx = start_parts.first().unwrap();
+                let sy = start_parts.get(1).unwrap();
+                let ex = end_parts.first().unwrap();
+                let ey = end_parts.get(1).unwrap();
+                for x in *min(sx, ex)..=*max(sx, ex) {
+                    for y in *min(sy, ey)..=*max(sy, ey) {
+                        grid.entry((x, y))
+                            .and_modify(|v| *v = false)
+                            .or_insert(false);
+                    }
+                }
+            }
+            _ => {
+                let start = command_parts[1];
+                let end = command_parts[3];
+                let start_parts = start
+                    .split(',')
+                    .map(|a| a.parse::<u32>().unwrap())
+                    .collect::<Vec<u32>>();
+                let end_parts = end
+                    .split(',')
+                    .map(|a| a.parse::<u32>().unwrap())
+                    .collect::<Vec<u32>>();
+                let sx = start_parts.first().unwrap();
+                let sy = start_parts.get(1).unwrap();
+                let ex = end_parts.first().unwrap();
+                let ey = end_parts.get(1).unwrap();
+                for x in *min(sx, ex)..=*max(sx, ex) {
+                    for y in *min(sy, ey)..=*max(sy, ey) {
+                        grid.entry((x, y)).and_modify(|v| *v = !*v).or_insert(true);
+                    }
+                }
+            }
+        }
     });
 
-    0
+    grid.values().filter(|v| **v).count()
 }
 
-fn b(_path: &str) -> u32 {
-    0
+fn b(path: &str) -> u32 {
+    let mut grid: HashMap<(u32, u32), u32> = HashMap::new();
+    read_lines(path).unwrap().iter().for_each(|line| {
+        let command_parts = line.split(' ').collect::<Vec<&str>>();
+        let command = command_parts.get(1).unwrap();
+        match *command {
+            "on" => {
+                let start = command_parts[2];
+                let end = command_parts[4];
+                let start_parts = start
+                    .split(',')
+                    .map(|a| a.parse::<u32>().unwrap())
+                    .collect::<Vec<u32>>();
+                let end_parts = end
+                    .split(',')
+                    .map(|a| a.parse::<u32>().unwrap())
+                    .collect::<Vec<u32>>();
+                let sx = start_parts.first().unwrap();
+                let sy = start_parts.get(1).unwrap();
+                let ex = end_parts.first().unwrap();
+                let ey = end_parts.get(1).unwrap();
+                for x in *min(sx, ex)..=*max(sx, ex) {
+                    for y in *min(sy, ey)..=*max(sy, ey) {
+                        grid.entry((x, y)).and_modify(|v| *v += 1).or_insert(1);
+                    }
+                }
+            }
+            "off" => {
+                let start = command_parts[2];
+                let end = command_parts[4];
+                let start_parts = start
+                    .split(',')
+                    .map(|a| a.parse::<u32>().unwrap())
+                    .collect::<Vec<u32>>();
+                let end_parts = end
+                    .split(',')
+                    .map(|a| a.parse::<u32>().unwrap())
+                    .collect::<Vec<u32>>();
+                let sx = start_parts.first().unwrap();
+                let sy = start_parts.get(1).unwrap();
+                let ex = end_parts.first().unwrap();
+                let ey = end_parts.get(1).unwrap();
+                for x in *min(sx, ex)..=*max(sx, ex) {
+                    for y in *min(sy, ey)..=*max(sy, ey) {
+                        grid.entry((x, y))
+                            .and_modify(|v| {
+                                if *v > 0 {
+                                    *v -= 1
+                                }
+                            })
+                            .or_insert(0);
+                    }
+                }
+            }
+            _ => {
+                let start = command_parts[1];
+                let end = command_parts[3];
+                let start_parts = start
+                    .split(',')
+                    .map(|a| a.parse::<u32>().unwrap())
+                    .collect::<Vec<u32>>();
+                let end_parts = end
+                    .split(',')
+                    .map(|a| a.parse::<u32>().unwrap())
+                    .collect::<Vec<u32>>();
+                let sx = start_parts.first().unwrap();
+                let sy = start_parts.get(1).unwrap();
+                let ex = end_parts.first().unwrap();
+                let ey = end_parts.get(1).unwrap();
+                for x in *min(sx, ex)..=*max(sx, ex) {
+                    for y in *min(sy, ey)..=*max(sy, ey) {
+                        grid.entry((x, y)).and_modify(|v| *v += 2).or_insert(2);
+                    }
+                }
+            }
+        }
+    });
+
+    grid.values().sum()
 }
 
 #[cfg(test)]
@@ -28,14 +177,14 @@ mod tests {
 
     #[test]
     fn test_a() {
-        let test_path = "test-resources/day11.txt";
+        let test_path = "test-resources/day6.txt";
         let result = a(test_path);
-        assert_eq!(result, 157);
+        assert_eq!(result, 998_996);
     }
 
     #[test]
     fn test_b() {
-        let test_path = "test-resources/day11.txt";
+        let test_path = "test-resources/day6.txt";
         let result = b(test_path);
         assert_eq!(result, 70);
     }
